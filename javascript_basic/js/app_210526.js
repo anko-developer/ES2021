0b1541617d1ae6ab6518b1c83fc076034f90c35f
김명욱 <mikkeller@naver.com>
2021-05-27 14:17:13 +0900

    타입변환

--- javascript_basic/js/app_210526.js
+++ javascript_basic/js/app_210526.js
@@ -32,4 +32,39 @@
   console.log(+undefined); // NaN
   console.log(+{}); // NaN
   console.log(+(function(){})); NaN
+
+  // 명시적 문자열 타입 변환
+  console.log(String(1)); 
+  console.log(String(null));
+  console.log(String(undefined));
+
+  // 명시적 숫자열 타입 변환
+  console.log(Number('0'));
+  console.log(Number(true));
+  console.log(Number(false));
+
+  // 문자열 -> 숫자 타입 변환
+  console.log(parseInt('0'));
+  console.log(parseFloat('10.53'));
+
+  // * 연산자로 숫자 타입 변환
+  console.log('0' * 1);
+  console.log('10.53' * 1);
+  console.log(true * 1);
+
+  // 숫자열을 불리언 타입 변환
+  console.log(Boolean(0)); // false
+  console.log(Boolean(1)); // true
+  
+  // ! 부정 논리 연산자를 두번 사용하여 불리언 타입 변환
+  console.log(!!'x'); // 문자열을 불리언 타입으로 = true
+  console.log(!!''); // 문자열을 불리언 타입으로 = false
+  console.log(!!0); // 숫자열을 불리언 타입으로 = false
+  console.log(!!1); // 숫자열을 불리언 타입으로 = true
+  console.log(!!NaN); // 숫자열을 불리언 타입으로 = false
+  console.log(!!Infinity); // 숫자열을 불리언 타입으로 = true
+  console.log(!!null); // false
+  console.log(!!undefined); // false
+  console.log(!!{}); // 객체 타입을 불리언 타입으로 = true
+  console.log(!![]); // 객체 타입을 불리언 타입으로 = true
 }());
