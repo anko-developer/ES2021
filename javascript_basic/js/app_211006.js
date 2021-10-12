3f2a6f31f37ee6c1f1c1118b3ab6832f40453e22
it1403 <anko@gsitm.com>
2021-10-12 15:51:52 +0900

    test

--- javascript_basic/js/app_211006.js
+++ javascript_basic/js/app_211006.js
@@ -1,1 +1,24 @@
-(function () {})();
+(function () {
+  const Person = (function () {
+    const Person = function (name) {
+      this.name = name;
+    };
+
+    Person.prototype = {
+      constructor: Person,
+      say() {
+        console.log(`${this.name} 안녕?`);
+      }
+    };
+
+    return Person;
+  })();
+
+  const test = new Person('kim');
+
+  const wow = {
+    name: '하하'
+  };
+  
+  test.say.call(wow);
+})();
