59e05d06b197a0fccfb4e7c6838548e44c746ce8
김명욱 <mikkeller@naver.com>
2022-03-15 14:24:02 +0900

    js minimize add

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
@@ -29,31 +30,31 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/
       },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-        options: {
-          sources: {
-            list: [
-              {
-                tag: 'img',
-                attribute: 'src',
-                type: 'src'
-              }
-            ]
-          }
-        }
-      },
+      // {
+      //   test: /\.html$/i,
+      //   loader: 'html-loader',
+      //   options: {
+      //     sources: {
+      //       list: [
+      //         {
+      //           tag: 'img',
+      //           attribute: 'src',
+      //           type: 'src'
+      //         }
+      //       ]
+      //     }
+      //   }
+      // },
       {
         test: /\.(png|jpe?g|gif|svg|webp)$/i,
-        type: 'asset/resource',
+        type: 'asset',
         generator: {
           filename: 'assets/images/[name][ext]'
         }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: 'asset',
         generator: {
           filename: 'assets/fonts/[name][ext]'
         }
@@ -61,7 +62,7 @@ module.exports = {
     ]
   },
   optimization: {
-    minimizer: [new CssMinimizerPlugin()],
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
     // 개발 중에도 실행하려면 해당 minimize 값을 넣어준다.
     minimize: true
   },
@@ -96,12 +97,12 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
-    }),
-    new HtmlWebpackPlugin({
-      title: 'test',
-      // index.html 템플릿을 기반으로 빌드 결과물을 추가해준다.
-      template: path.join(path.resolve(__dirname, 'src'), 'index.html')
     })
+    // new HtmlWebpackPlugin({
+    //   title: 'test',
+    //   // index.html 템플릿을 기반으로 빌드 결과물을 추가해준다.
+    //   template: path.join(path.resolve(__dirname, 'src'), 'index.html')
+    // })
   ],
   devtool: 'source-map'
 };
