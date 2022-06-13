abb696ac444fbe57d91d648d3611be7ed16071f1
김명욱 <anko@gsitm.com>
2022-06-13 21:16:44 +0900

    prototype clear

--- javascriptDeepdive/2022/13_prototype.js
+++ javascriptDeepdive/2022/13_prototype.js
@@ -30,4 +30,70 @@
   console.log(me.test.call(good));
 
 
+
+
+
+
+  const item = document.querySelectorAll('.item');
+  const item1 = document.getElementsByClassName('item');
+  const test00 = [
+    {
+      name: 1
+    },
+    {
+      num: 2
+    }
+  ]
+  // Array.prototype.forEach.call(item, (el, index) => {
+  //   console.log(el);
+  // });
+
+  console.log(item1);
+  console.log(test00);
+
+  Array.from(item).forEach(el => {
+    console.log(el);
+  });
+
+  Array.prototype.forEach.call(item1, (el, index) => {
+    console.log(el);
+  });
+
+  test00.forEach((el, index) => {
+    console.log(el);
+  });
+
+
+
+  const Anko = (function() {
+    function Anko(name) {
+      this.name = name;
+    }
+
+    Anko.prototype = {
+      constructor: Anko,
+      sayHello() {
+        console.log(`Hi! My name is ${this.name}`);
+      }
+    };
+
+    return Anko;
+  }());
+
+  const you = new Anko('wook');
+  
+  console.log(you.constructor === Anko);
+
+
+  const ming = {
+    2: 2,
+    3: 3,
+    a: '와',
+    b: '후'
+  };
+
+  for (const key in ming) {
+    if (!ming.hasOwnProperty(key)) continue;
+    console.log(key + ':' + ming[key]);
+  }
 })();
