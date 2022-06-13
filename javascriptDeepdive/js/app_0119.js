8beb2b74440472856d17847016cc516ea95dd7b8
김명욱 <anko@gsitm.com>
2022-06-13 10:26:35 +0900

    설명 수정

--- javascriptDeepdive/js/app_0119.js
+++ javascriptDeepdive/js/app_0119.js
@@ -5,17 +5,17 @@
   const listItem = document.getElementsByClassName('study__item'); 
   const listItem1 = document.querySelectorAll('.study__item');
 
-  // 방법1
+  // 방법1 - Array.prototype에 접근하여 call로 바인딩 시켜주기
   Array.prototype.forEach.call(listItem, (elem, index) => {
     console.log(elem, index);
   });
 
-  // 방법2
+  // 방법2 - 스프레드 사용
   [...listItem].forEach((elem, index) => {
     console.log(elem, index);
   });
 
-  // 방법3 - Array.from 메서드를 사용하면 NodeList나 HTMLCollection을 순수 배열로 반환 받을 수 있다
+  // 방법3 - Array.from 메서드를 사용하면 NodeList나 HTMLCollection같은 유사배열객체를 순수 배열로 반환 받을 수 있다
   Array.from(listItem1).forEach((elem, index) => {
     console.log(index);
   });
