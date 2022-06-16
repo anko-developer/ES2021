6577f1f67e49daa3471b23f6dec93bef853d38d4
김명욱 <anko@gsitm.com>
2022-06-16 20:59:39 +0900

    test

--- javascriptDeepdive/js/app.js
+++ javascriptDeepdive/js/app.js
@@ -1,74 +1,17 @@
-(function() { // 즉시 실행 함수로 스크립트 전체를 감싸서 스코프를 구분하고
-  'use strict'; // 즉시 실행 함수의 선두에 strict mode를 적용한다. 
+(function() {
+  function Person(test) {
+    this.test = test;
+  }
 
-  // let str = '';
+  Person.prototype = {
+    constructor: Person,
+    gogo() {
+      console.log(this.test);
+    },
+    num: 1,
+    string: '문자열.'
+  };
 
-  /**
-   * Quiz.01
-   * *
-   * **
-   * ***
-   * ****
-   * *****
-   * ****
-   * ***
-   * **
-   * *
-   */
-
-  // for(let i = 1; i <= 10; i++) {
-  //   // console.log(str.repeat(i));
-  //   str += '*';
-  //   if (i % 2 === 0) {
-  //     continue;
-  //   } else {
-  //     console.log(str);
-  //   }
-    
-  // }
-
-  // for(let i = 10; i >= 1; i--) {
-  //   // console.log(str.repeat(i));
-  //   if (i % 2 === 0) {
-  //     continue;
-  //   } else {
-  //     console.log(str.substring(0, i));
-  //   }
-  // }
-
-  /**
-   * Quiz.02
-   * *****
-   *  ****
-   *   ***
-   *    **
-   *     *
-   */
-  
-  // for (let i = 4; i >= 0; i--) {
-  //   for (let ii = 1; ii <= 5; ii++) {
-  //     if (i + ii === 5) {
-  //       console.log(' '.repeat(i) + '*'.repeat(ii));
-  //     }
-  //   }
-  // }
-
-  /**
-   * Quiz.03
-   * 배열에서 '라' 문자열을 모두 없애시오.
-   */
-
-  // const arr = ['가', '라', '나', '라', '다', '라'];
-
-  // while (arr.join('').indexOf('라') > -1) {
-  //   const i = arr.join('').indexOf('라');
-  //   arr.splice(i, 1);
-
-  //   console.log(arr);
-  // }
-  // const multily = (x,y,z) => {
-  //   return console.log((x * y) * z);
-  // }
-
-  // multily(1,2,3);
-}());
+  const i = new Person('test');
+  i.gogo();
+})();
