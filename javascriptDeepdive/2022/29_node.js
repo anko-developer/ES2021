0fa3bf707d8bd3b0257127fd783a567a2c409d23
김명욱 <anko@gsitm.com>
2022-09-16 21:28:20 +0900

    to rest

--- javascriptDeepdive/2022/29_node.js
+++ javascriptDeepdive/2022/29_node.js
@@ -1,6 +1,6 @@
 (function() {
   // NodeList, HTMLCollection은 유사 배열이면서 이터러블이기 때문에
-  // 스프레드 문법으로 배열로 바꿀 수 있다. 
+  // 스프레드 문법으로 배열로 바꿀 수 있다.   
   // Array에 유용한 고차함수가 많기 때문에 배열로 변환해서 사용하면 좋다.
   const $li = document.querySelectorAll('.item');
   [...$li].forEach((item) => {
