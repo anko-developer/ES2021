1f22b84dfe11d5701ca86a147a127726fc289225
김명욱 <anko@gsitm.com>
2022-09-28 12:57:14 +0900

    비동기처리

---
+++ javascriptDeepdive/2022/34_callstack.js
@@ -0,0 +1,13 @@
+(function() {
+  // setTimeout 는 테스크큐에 푸시되어 있다가 콜 스택이 비게되면 비로소 콜스택에 푸시되어 실행된다.
+  const foo = () => {
+    console.log('foo');
+  };
+
+  const bar = () => {
+    console.log('bar');
+  };
+
+  setTimeout(foo, 0);
+  bar();
+}());
