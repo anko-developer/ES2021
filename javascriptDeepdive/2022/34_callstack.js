948a687885fee1811202040864b23910745236df
김명욱 <anko@gsitm.com>
2022-09-29 18:58:41 +0900

    to rest

--- javascriptDeepdive/2022/34_callstack.js
+++ javascriptDeepdive/2022/34_callstack.js
@@ -1,5 +1,5 @@
 (function() {
-  // setTimeout 는 테스크큐에 푸시되어 있다가 콜 스택이 비게되면 비로소 콜스택에 푸시되어 실행된다.
+  // setTimeout 는 테스크큐에 푸시되어 있다가 콜 스택이 비게되면 비로소 콜스택에 푸시되어 실행된다. 
   const foo = () => {
     console.log('foo');
   };
