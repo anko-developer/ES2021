7dbc1f3cf20a6f4580f75e6c791114139a70f3f3
김명욱 <anko@gsitm.com>
2022-10-15 11:15:20 +0900

    regExp review

---
+++ javascriptDeepdive/studyReview/regExp.js
@@ -0,0 +1,18 @@
+(function() {
+  const target = 'color colour';
+  const regExp = /colou?r/g;
+  console.log(target.match(regExp));
+
+  const target1 = 'AA BB 12,345'
+  const regExp1 = /[0-9]+/g;
+  const regExp2 = /[0-9,]+/g;
+  const regExp3 = /[\d,]+/g;
+  const regExp4 = /[\D,]+/g;
+  console.log(target1.match(regExp1));
+  console.log(target1.match(regExp2));
+  console.log(target1.match(regExp3));
+  console.log(target1.match(regExp4));
+
+  const regExp5 = /[^A]+/g;
+  console.log(target1.match(regExp5));
+}());
