6b4c1efc88da46b0a8667ae5deaec8b00d52fd9f
김명욱 <anko@gsitm.com>
2022-10-17 15:54:44 +0900

    regExp clear

--- javascriptDeepdive/studyReview/regExp.js
+++ javascriptDeepdive/studyReview/regExp.js
@@ -14,5 +14,24 @@
   console.log(target1.match(regExp4));
 
   const regExp5 = /[^A]+/g;
-  console.log(target1.match(regExp5)); 
+  console.log(target1.match(regExp5));
+
+  const url = 'https://example.com';
+  console.log(/^https?:\/\//g.test(url));
+
+  const fileName = 'index.html';
+  console.log(/html$/g.test(fileName));
+
+  const target2 = '12345';
+  console.log(/^\d+$/g.test(target2));
+
+  const id = 'abc123';
+  console.log(/^[A-Za-z0-9]{4,10}$/g.test(id));
+
+  const cellphone = '010-1234-5678';
+  console.log(/^\d{3}-\d{3,4}-\d{4}$/g.test(cellphone));
+
+  const target3 = 'abc#123';
+  console.log(/[^A-Za-z0-9]/ig.test(target3));
+  console.log(target3.replace(/[^A-Za-z0-9]/ig, ''));
 }());
