042b8bf374cc6e799542bbcfddec3013c10d647e
김명욱 <anko@gsitm.com>
2022-11-21 14:07:35 +0900

    document object model

---
+++ javascriptDeepdive/studyReview/dom.js
@@ -0,0 +1,14 @@
+(function() {
+  const fruitsList = document.querySelectorAll('#fruits li');
+  const test = [...fruitsList];
+  test.forEach((item, index) => {
+    if (item.className === 'orange') {
+      test.splice(index, 1, 'test');
+    }
+  });
+  
+  const fruits = document.getElementById('fruits');
+  const li = document.createElement('li');
+  li.textContent = 'test';
+  fruits.insertBefore(li, fruits.lastElementChild);
+}());
