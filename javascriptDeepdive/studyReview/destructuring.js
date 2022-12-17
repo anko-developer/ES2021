d52f7a87aaf86ef8b37128ed3f813e3c438a6cec
김명욱 <anko@gsitm.com>
2022-12-17 22:17:42 +0900

    to rest

--- javascriptDeepdive/studyReview/destructuring.js
+++ javascriptDeepdive/studyReview/destructuring.js
@@ -17,7 +17,7 @@
   console.log(length);
 
   function printTodo({ content, completed }) {
-    console.log(`할일 ${content}은 ${completed ? '완료' : '미완료'}입니다.`);
+    console.log(`할일 ${content}은 ${completed ? '완료' : '미완료'}입니다!`);
   }
 
   printTodo({ content: 'JavaScript', completed: false });
