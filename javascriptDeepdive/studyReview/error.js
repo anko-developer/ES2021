c22bb90e1b8a9abf0e67321796e08e2c897cd8b8
김명욱 <anko@gsitm.com>
2023-01-07 17:17:00 +0900

    error 처리

---
+++ javascriptDeepdive/studyReview/error.js
@@ -0,0 +1,14 @@
+(function() {
+  // 옵셔널 체이닝
+  const $button = document.querySelector('button');
+  $button?.classList.add('why');
+
+  // try catch
+  try {
+    
+  } catch (error) {
+    
+  } finally {
+    
+  }
+}());
