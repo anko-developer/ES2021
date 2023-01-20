38cbe575dd993fced9ce5d1635657ec62f4b8b46
김명욱 <anko@gsitm.com>
2023-01-20 20:21:28 +0900

    비동기처리

---
+++ javascriptDeepdive/studyReview/asyncawait3.js
@@ -0,0 +1,35 @@
+function delayP(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve('테스트입니다'), ms);
+    reject(error);
+  }); 
+};
+
+delayP(5000).then(result => {
+    console.log(result);
+})
+
+function a () {
+  return new Promise((resolve, reject) => {
+    resolve('test');
+  });
+}
+
+a()
+  .then((response) => {
+    console.log(response);
+    setTimeout(response, 3000);
+  })
+  .then(response => console.log('gogogo'))
+  .catch((err) => console.log(err));
+
+
+
+// async function start () {
+//   const a = await delayP(100000);
+//   console.log('a', a);
+//   const b = await delayP(3000);
+//   console.log('b', b);
+// };
+
+// start();
